33f9cc713206f68234059a2dd3c5ebd3d482e892
Dumisanelinqa447 <Dumisanelinqa447@gmail.com>
2026-04-16 11:52:34 +0200

    Create file-organizer.py

---
+++ file-organizer.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+
+path = "C:/Users/YourName/Downloads"
+
+files = os.listdir(path)
+
+for file in files:
+    if file.endswith(".jpg") or file.endswith(".png"):
+        shutil.move(path + "/" + file, path + "/Images/" + file)
+    elif file.endswith(".pdf") or file.endswith(".docx"):
+        shutil.move(path + "/" + file, path + "/Documents/" + file)
